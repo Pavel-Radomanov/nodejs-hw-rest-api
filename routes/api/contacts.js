fd8b1343fa8d93b72ff5dec790b4ca64dd702f12
Павло Радоманов <radomanov.pavel@gmail.com>
2023-06-12 20:27:07 +0300

    add function Remove

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -56,7 +56,17 @@ router.post("/", async (req, res, next) => {
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.removeContact(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json({ message: "Delete success" });
+    // res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
