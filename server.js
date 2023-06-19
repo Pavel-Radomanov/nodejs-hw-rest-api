fd6450be5d213236a089894d73133ce651f57343
Павло Радоманов <radomanov.pavel@gmail.com>
2023-06-19 15:56:38 +0300

    done connect mongoose

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const MONGO_URL =
+  "mongodb+srv://tryconnect:5dZch%24epw%40bs!.z@cluster0.fuqz6wh.mongodb.net/";
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(MONGO_URL)
+
+  .then(() => {
+    console.log("MongoDB successfuly connected");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
